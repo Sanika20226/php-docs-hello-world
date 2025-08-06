15a5f0379a45e0dea84f743ef9f8d1efbfdd5704
Sanika20226 <Sanika.Bobade23@iccs.ac.in>
2025-08-06 11:40:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-
-echo "Hello Sanika!";
+echo"your name is:".$_GET["fullname"];
+echo"<br>";
+echo"your email is:".$_GET["email"];
+echo"<br>";
+echo"your age is:".$_GET["age"];
+echo"<br>";
+echo"your gender is:".$_GET["gender"];
+echo"<br>";
+echo"your course is:".$_GET["course"];
+echo"<br>";
+?>
